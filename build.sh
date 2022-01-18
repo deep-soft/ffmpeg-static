ff7654e64b625e0074baf4a61ecd1996ed887d75
HMH <henry@freedesk.net>
2022-01-18 17:25:23 +0100

    ffmpeg 5.0.

--- build.sh
+++ build.sh
@@ -2,27 +2,74 @@
 
 set -ex
 
+export TARGET_OS="$RUNNER_OS"
+
 if [ "$OSTYPE" == "linux-gnu" ]; then
-    export RUNNER_OS="Linux"
+    export HOST_OS="linux"
 fi
 
-export FFMPEG_EXTRA_ARGS="--disable-nvenc --disable-ffnvcodec"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export HOST_OS="macos"
+    export MACOSX_DEPLOYMENT_TARGET="10.13"
+fi
 
-if [ "$RUNNER_OS" == "Windows" ]; then
-    export CC=cl
-    export FFMPEG_EXTRA_ARGS="$FFMPEG_EXTRA_ARGS --toolchain=msvc"
-    export FFMPEG_CFLAGS="-I../dist/include"
-    export FFMPEG_LIBRARY_PATH="-LIBPATH:../dist/lib"
+if [ "$OS" == "Windows_NT" ]; then
+    export HOST_OS="windows"
+fi
+
+[ -z "$TARGET_OS" ] && export TARGET_OS="$HOST_OS"
+
+export NPROCS="$(nproc || echo 4)"
+
+./download.sh
+
+if [ "$TARGET_OS" == "windows" ]; then
+    if [ "$HOST_OS" == "linux" ]; then
+        export CROSS_COMPILE="x86_64-w64-mingw32-"
+        export FFMPEG_EXTRA_ARGS="--arch=x86_64 --target-os=mingw64 --cross-prefix=x86_64-w64-mingw32- \
+            --enable-nvenc --enable-ffnvcodec --enable-mediafoundation --pkg-config=pkg-config"
+        export FFMPEG_CFLAGS="-I../dist/include"
+        export FFMPEG_LIBRARY_PATH="-L../dist/lib"
+    else
+        export CC="cl"
+        export FFMPEG_EXTRA_ARGS="--toolchain=msvc --enable-nvenc --enable-ffnvcodec \
+            --enable-mediafoundation"
+        export FFMPEG_CFLAGS="-I../dist/include"
+        export FFMPEG_LIBRARY_PATH="-LIBPATH:../dist/lib"
+    fi
 else
     export FFMPEG_CFLAGS="-I../dist/include"
     export FFMPEG_LIBRARY_PATH="-L../dist/lib"
-    if [ "$RUNNER_OS" == "Linux" ]; then
-        export FFMPEG_EXTRA_ARGS="--enable-nvenc --enable-ffnvcodec"
+    if [ "$TARGET_OS" == "linux" ]; then
+        export FFMPEG_EXTRA_ARGS="--enable-nvenc \
+            --enable-ffnvcodec \
+            --enable-vaapi \
+            --enable-libdrm \
+            --enable-xlib"
+    fi
+    if [ "$TARGET_OS" == "macos" ]; then
+        export FFMPEG_EXTRA_ARGS="--enable-videotoolbox"
     fi
 fi
 
+if [ "$TARGET_OS" == "windows" ] && [ "$HOST_OS" == "linux" ]; then
+    export X264_EXTRA_ARGS="--cross-prefix=x86_64-w64-mingw32- --host=x86_64-w64-mingw32"
+fi
 ./x264.sh
-if [ "$RUNNER_OS" == "Linux" ]; then
+if [ "$TARGET_OS" == "linux" ]; then
+    ./nv-codec-headers.sh
+    ./libva.sh
+fi
+if [ "$TARGET_OS" == "windows" ]; then
     ./nv-codec-headers.sh
 fi
 ./ffmpeg.sh
+
+if [ "$TARGET_OS" == "windows" ] && [ "$HOST_OS" == "windows" ]; then
+    cd dist/lib
+    for l in *.a; do
+        d=${l#lib}
+        cp "$l" "${d%.a}.lib"
+    done
+    cp libx264.lib x264.lib
+fi
--- download.sh
+++ download.sh
@@ -2,18 +2,19 @@
 
 set -ex
 
-if [ "$OSTYPE" == "linux-gnu" ]; then
-    export RUNNER_OS="Linux"
+rm -rf x264 ffmpeg
+git clone --depth 1 -b stable https://code.videolan.org/videolan/x264.git x264
+git clone --depth 1 -b n5.0 https://git.ffmpeg.org/ffmpeg.git ffmpeg
+if [ "$TARGET_OS" == "linux" ]; then
+    git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+    git clone --depth 1 -b 2.13.0 https://github.com/intel/libva
 fi
-
-git clone -b stable https://code.videolan.org/videolan/x264.git x264
-git clone -b n4.2.3 https://git.ffmpeg.org/ffmpeg.git ffmpeg
-if [ "$RUNNER_OS" == "Linux" ]; then
-    git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+if [ "$TARGET_OS" == "windows" ]; then
+    git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 fi
-
 cd ffmpeg
-if [ "$RUNNER_OS" == "Windows" ]; then
+
+if [ "$TARGET_OS" == "windows" ] && [ "$HOST_OS" == "windows" ]; then
     git apply ../command_limit.patch
     git apply ../awk.patch
     git apply ../ffmpeg-x264-static.patch
